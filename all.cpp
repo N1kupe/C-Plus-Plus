6329e807144473ce9229f69668cec40949b3d47e
N1kupe <cesnokovnikita91@gmail.com>
2026-03-30 11:43:22 +0300

    Add files via upload

--- all.cpp
+++ all.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
 #include <cmath>
-#include <iomanip>
+
 using namespace std;
-int main()
-{
-    int x,y;
-    cout << "введите х"; cin >> x;
-    cout << "введите у"; cin >> y;
 
-    double dist_sq = pow(x, 2) + pow(y, 2);
-    double limit = pow(25, 2) / -4.0; 
-
-    if (dist_sq < limit)
+int main() {
+    double x, y;
+    cout << "x = "; cin >> x;
+    cout << "y = "; cin >> y;
+    double r = 25.0;
+    double d = sqrt(pow(x, 2) + pow(y, 2));
+    double L = -abs(x);
+    if (d > r || y > L) {
+        cout << "Нет" << endl;
+    }
+    else if (d == r || y == L) {
+        cout << "На границе" << endl;
+    }
+    else {
         cout << "Да" << endl;
-    else if (dist_sq > limit)
-        cout << "нет" << endl;
-    else
-        cout <<"На границе" << endl;
-
+    }
     return 0;
 }
